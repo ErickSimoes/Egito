e0965945d1db75263d42b363a1df022293cb030b
alaurentinoofficial <alaurentino.br@gmail.com>
2017-10-21 20:30:30 -0200

    Recommit

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+
+	Vector3 localScale;
+	Vector3 localPosition;
+
+	private void Start() {
+		localScale = transform.localScale;
+		localPosition = transform.localPosition;
+	}
+
+	public void Click() {
+		GameController.player.transform.position = localPosition;
+	}
+
+	public void MouseHover(bool enter) {
+		if (enter) {
+			transform.localScale = new Vector3(.1f, .1f, .1f) + localScale;
+			GetComponent<ParticleSystem>().Play();
+		} else {
+			transform.localScale = localScale;
+			GetComponent<ParticleSystem>().Stop();
+		}
+	}
+
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.gameObject.CompareTag ("Player"))
+			gameObject.SetActive (false);
+	}
+
+	void OnTriggerExit(Collider c)
+	{
+		if(c.gameObject.CompareTag("Player"))
+			gameObject.SetActive (true);
+	}
+}
